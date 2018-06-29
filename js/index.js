68e626ef10cb263b2816ed1a2d3d9fb5b753933e
fuyanshuang <1048404603@qq.com>
2018-06-29 15:14:07 +0800

    this is the secont commit by 6.29

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 $(function(){
-	/*$(".banner_left").hover(function(){
-		$(this).find("li").css("backgroundColor", "#f7f7f7");
-	},
-	function(){
-		$(this).find("li").css("backgroundColor", "#fff");
-	})*/
+	
 
 	$(".banner_left_li").hover(function(){
 		$(this).css("backgroundColor", "#fff")
@@ -25,7 +20,7 @@ $(function(){
 
 
 	/*  轮播图   */
-	function banner_top(){
+	function banner_c_top(){
 		var banner_top_images = $(".banner_sowing_a");
 		var banner_top_spans = $(".banner_sowing_o").find("span");
 
@@ -70,10 +65,19 @@ $(function(){
 				}, 3000);
 			}
 		}
+
+		$(".banner_sowing_a").hover(function(){
+			clearInterval(imageInitailMove);
+		},function(){
+			imageInitailMove = setInterval(function(){
+					imageMove();
+				}, 3000);
+		})
+
 	}
-	banner_top();
+	banner_c_top();
 	
-	function banner_bottom(){
+	function banner_c_bottom(){
 		var oBcbul = $(".b_c_bul");
 		var oBcsleft = $(".b_c_bsleft");
 		var oBcsright = $(".b_c_bsright");
@@ -84,12 +88,15 @@ $(function(){
 			oBcbul.animate({
 				left: -744 * iBnow
 			}, 800, function(){
-
 				if(iBnow == 2){
 					iBnow = 0;
 					oBcbul.css("left", 0);
 				}
 			})
+
+			/*$(".b_c_bsleft").click(function(){
+				oBcbul.style.left = oBcbul.offsetLeft + "744" + "px";
+			})*/
 		}
 
 		function timerBcbulInner(){
@@ -99,14 +106,89 @@ $(function(){
 
 		timerBcbul = setInterval(timerBcbulInner, 3000);
 
-
 	
-		/*$(".b_c_bul").hover(function(){
-			clearInterval(timerBcbulInner);
+		$(".b_c_bmover").hover(function(){
+			clearInterval(timerBcbul);
 		},
 		function(){ 
 			timerBcbul = setInterval(timerBcbulInner, 3000);
-		})*/
+		})
 	}
-	banner_bottom();
+	banner_c_bottom();
+
+
+
+	function banner_r_top(){
+		var oUl = $(".b_r_tul");
+		var aSpans = $(".b_r_tspan").find("span");
+
+		var iUnow = 0;
+		var timerBrtul = null;
+
+		function brtulRoll(){
+			aSpans.removeClass("brtspans").eq(iUnow).addClass("brtspans");
+			if(iUnow == 2){
+				aSpans.eq(0).addClass("brtspans");
+			}
+			oUl.animate({
+				left: -226 * iUnow
+			}, 400, function(){
+				if(iUnow == 2){
+					iUnow = 0;
+					oUl.css("left", 0);
+				}
+			});
+
+			for(var i = 0; i < aSpans.length; i++){
+
+			}
+		}
+
+		function timerBrtulInnter(){
+			iUnow++;
+			brtulRoll();
+		}
+
+		timerBrtul = setInterval(timerBrtulInnter, 2000);
+
+		aSpans.click(function(){
+			iUnow = $(this).index();
+			brtulRoll();
+		})
+
+		$(".b_r_tul").hover(function(){
+			clearInterval(timerBrtul);
+		},
+		function(){
+			timerBrtul = setInterval(timerBrtulInnter, 2000);
+		})
+	}
+	banner_r_top();
+
+
+	function hotsale_roll(){
+		var oP = $(".hotsale_title").find("p");
+		var oUl = $(".hotsale_left_ul");
+		var iNow = 0;
+		var timer = null;
+		var count = 1;
+
+		$(".hotsale_title").find("p").click(function(){
+			
+			
+			oUl.animate({
+				left: -960 * count
+			}, 200, function(){
+				if(count == 4){
+					
+					oUl.animate({
+						left: 0
+					}, 0)
+					count = 1;
+				}
+			})
+			count++;
+		})
+	}
+	hotsale_roll();
 })
